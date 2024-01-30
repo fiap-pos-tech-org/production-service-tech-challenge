4a3544ca2df681c71ab60ee4050a1fd9b1a95ab1
diego <oliveiraj.diego@gmail.com>
2024-01-30 14:50:27 -0300

    chore: remove useless files and configs

--- src/test/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO produto (id, nome, categoria, preco, descricao)
-VALUES (1, 'Hamburguer Angus', 'LANCHE', 25.99, 'Hamburguer Angus'),
-       (2, 'Hamburguer X-Bacon', 'LANCHE', 24.98, 'Hamburguer X-Bacon'),
-       (3, 'Hamburguer X-Salada', 'LANCHE', 23.89, 'Hamburguer X-Salada'),
-       (4, 'Suco de Abacaxi', 'BEBIDA', 10.50, 'Suco de Abacaxi'),
-       (5, 'Coca Cola', 'BEBIDA', 12.50, 'Coca Cola');
