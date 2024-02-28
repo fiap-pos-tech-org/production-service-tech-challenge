0a94edc32e3db09433d986f6ebe05884e718f578
Pedro Pereira <pedrojr.19@hotmail.com>
2024-02-28 09:51:35 -0300

    fix: X-Content-Type-Options Header Missing

---
+++ src/main/java/br/com/fiap/techchallenge/production/config/ContentTypeOptionsFilter.java
@@ -0,0 +1,20 @@
+package br.com.fiap.techchallenge.production.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+public class ContentTypeOptionsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("X-Content-Type-Options", "nosniff");
+        filterChain.doFilter(request, response);
+    }
+}
