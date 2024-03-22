f8230dc4a7e10c0e21d3b3340e0469659e9605ab
diego <oliveiraj.diego@gmail.com>
2024-03-22 08:37:41 -0300

    test: add h2 dependency

--- src/main/java/br/com/fiap/techchallenge/production/ProductionApplication.java
+++ src/main/java/br/com/fiap/techchallenge/production/ProductionApplication.java
@@ -2,9 +2,8 @@ package br.com.fiap.techchallenge.production;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class ProductionApplication {
 
     public static void main(String[] args) {
